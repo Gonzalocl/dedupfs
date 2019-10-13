24b40a8ea0e74b8e8c54baf89be2439db2dad6f9
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-10-14 01:13:21 +0200

    block handler definition

---
+++ fuse-tutorial-2018-02-04/src/dedupfs/block_handler.h
@@ -0,0 +1,45 @@
+#ifndef BLOCK_HANDLER_H
+#define BLOCK_HANDLER_H
+
+struct block_handler_conf {
+    char *blocks_path;
+    int block_size;
+    int hash_type;
+    int hash_split;
+};
+
+
+// pass an allocated struct
+int block_handler_init(struct block_handler_conf *conf);
+int block_handler_get_conf(struct block_handler_conf *conf);
+
+// Free blocks_path
+int block_handler_end();
+
+
+int block_create(char *hash, char *data);
+int block_delete(char *hash);
+
+
+int block_read(char *hash, char *data);
+
+// Doesn't make sense
+//int block_write(char *hash, char *data);
+
+// Could be useful
+//int block_read_delete(char *hash, char *data);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //BLOCK_HANDLER_H
