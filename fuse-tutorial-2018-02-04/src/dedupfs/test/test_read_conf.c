b2c000d0106b737fda605aa028b62448741c6f6a
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-10-26 01:23:21 +0200

    test wiht fopen fread

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_read_conf.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_read_conf.c
@@ -4,19 +4,43 @@
 
 #include "../block_handler.h"
 
-#define CONF_FILENAME "conf.txt"
+#define CONF_FILENAME "conf"
 struct block_handler_conf* handler_conf = NULL;
 
 int read_conf_file(char *blocks_path) {
+
+    // TODO use open read write?
+    // TODO max
+    char path[4096];
+    sprintf(path, "%s/%s", blocks_path, CONF_FILENAME);
+    printf("path: %s\n", path);
+
+    FILE * conf_file;
+    if ((conf_file = fopen(path, "rb")) == NULL) {
+        return -1;
+    }
+
+    int len;
+    fread(&len, 1, 1, conf_file);
+    fread(handler_conf->blocks_path, 1, len, conf_file);
+    fread(&handler_conf->block_size, 4, 1, conf_file);
+    fread(&handler_conf->hash_type, 4, 1, conf_file);
+    fread(&handler_conf->hash_length, 4, 1, conf_file);
+    fread(&handler_conf->hash_split, 4, 1, conf_file);
+    fread(&handler_conf->bytes_link_counter, 4, 1, conf_file);
+
+
+    fclose(conf_file);
+
     // TODO malloc str? set max
     // TODO dont use scanf
-    scanf("%s\n%d\n%d\n%d\n%d\n%d\n",
-            handler_conf->blocks_path,
-            &handler_conf->block_size,
-            &handler_conf->hash_type,
-            &handler_conf->hash_length,
-            &handler_conf->hash_split,
-            &handler_conf->bytes_link_counter);
+//    scanf("%s\n%d\n%d\n%d\n%d\n%d\n",
+//            handler_conf->blocks_path,
+//            &handler_conf->block_size,
+//            &handler_conf->hash_type,
+//            &handler_conf->hash_length,
+//            &handler_conf->hash_split,
+//            &handler_conf->bytes_link_counter);
 }
 
 
@@ -26,6 +50,7 @@ int main () {
     handler_conf->blocks_path = malloc(4096);
     read_conf_file("bin");
 
+    printf("\n");
     printf("blocks_path:        %s\n", handler_conf->blocks_path);
     printf("block_size:         %d\n", handler_conf->block_size);
     printf("hash_type:          %d\n", handler_conf->hash_type);
