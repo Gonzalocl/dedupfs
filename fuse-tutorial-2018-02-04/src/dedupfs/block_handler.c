d35d218f5b61a16fd5365121121e91ffbf3f2c5b
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-10-28 12:41:39 +0100

    copy read write functions

--- fuse-tutorial-2018-02-04/src/dedupfs/block_handler.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/block_handler.c
@@ -12,6 +12,145 @@
 struct block_handler_conf* handler_conf = NULL;
 char fs_path[PATH_MAX];
 
+/*
+ * create configuration file and write it
+ * the folder should exist handler_conf allocated and
+ * fs_path set
+ */
+int write_conf_file() {
+
+    // TODO use open read write?
+    char path[PATH_MAX];
+    int path_length;
+    int ret_value = 0;
+
+
+    if (snprintf(path, PATH_MAX, "%s/%s/%s", fs_path, handler_conf->blocks_path, CONF_FILENAME) < 0) {
+        return -EIO;
+    }
+
+    FILE * conf_file;
+    if ((conf_file = fopen(path, "wb")) == NULL) {
+        return -errno;
+    }
+
+    path_length = strnlen(handler_conf->blocks_path, PATH_MAX);
+    if (fwrite(&path_length, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fwrite(handler_conf->blocks_path, 1, path_length, conf_file) != path_length) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->block_size, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->hash_type, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->hash_length, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->hash_split, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->hash_split_size, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fwrite(&handler_conf->bytes_link_counter, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    out:
+    if (fclose(conf_file) != 0) {
+        return -errno;
+    }
+    return ret_value;
+}
+
+/*
+ * read configuration file
+ * file should exist handler_conf allocated and
+ * fs_path set
+ */
+int read_conf_file(char *blocks_path) {
+
+    // TODO use open read write?
+    char path[PATH_MAX];
+    int path_length;
+    int ret_value = 0;
+
+
+    if (snprintf(path, PATH_MAX, "%s/%s/%s", fs_path, blocks_path, CONF_FILENAME) < 0) {
+        return -EIO;
+    }
+
+    FILE * conf_file;
+    if ((conf_file = fopen(path, "rb")) == NULL) {
+        return -errno;
+    }
+
+    // TODO check this can be negative
+    if (fread(&path_length, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fread(handler_conf->blocks_path, 1, path_length, conf_file) != path_length) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    if (fread(&handler_conf->block_size, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (handler_conf->block_size <= 0) {
+        ret_value = -ENOTRECOVERABLE;
+        goto out;
+    }
+
+    // TODO check read values
+    if (fread(&handler_conf->hash_type, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fread(&handler_conf->hash_length, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fread(&handler_conf->hash_split, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fread(&handler_conf->hash_split_size, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+    if (fread(&handler_conf->bytes_link_counter, 4, 1, conf_file) != 1) {
+        ret_value = -EIO;
+        goto out;
+    }
+
+    out:
+    if (fclose(conf_file) != 0) {
+        return -errno;
+    }
+    return ret_value;
+}
+
 // pass an allocated struct
 int block_handler_init(struct block_handler_conf *conf) {
 
