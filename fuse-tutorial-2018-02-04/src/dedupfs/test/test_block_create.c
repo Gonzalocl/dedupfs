fca524bcd63f788570e3aef2530738be2c640e84
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-03 02:15:25 +0100

    test_block_create

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.c
@@ -1,17 +1,39 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "../block_handler.h"
 
-unsigned char hash[16] = {0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80, 0x90, 0xa0, 0xb0, 0xc0, 0xd0, 0xe0, 0xf0};
+int main (int argc, char *argv[]) {
 
+    if (argc != 10) {
+        printf("ERROR\n");
+        return 1;
+    }
 
-int main () {
+    struct block_handler_conf conf;
+    strcpy(conf.blocks_path, argv[1]);
+    conf.block_size = atoi(argv[2]);
+    conf.hash_type = atoi(argv[3]);
+    conf.hash_length = atoi(argv[4]);
+    conf.hash_split = atoi(argv[5]);
+    conf.hash_split_size = atoi(argv[6]);
+    conf.bytes_link_counter = atoi(argv[7]);
+    unsigned char hash[conf.hash_length];
+    unsigned char data[conf.block_size];
+
+    for (int i = 0; i < conf.hash_length; i++) {
+        sscanf(argv[8] + i*2, "%02hhx", &hash[i]);
+    }
+
+    strncpy(data, argv[9], conf.block_size);
+
     int ret;
-    ret = block_handler_init("bin/fs", NULL);
-//    printf("init: %d\n", ret);
-    ret = block_create(hash, NULL);
-//    printf("create: %d\n", ret);
+    ret = block_handler_init("bin/fs", &conf);
+    printf("init: %d\n", ret);
+    ret = block_create(hash, data);
+    printf("create: %d\n", ret);
 //    printf("%s:%s:%d\n", __FILE__, __func__, __LINE__);
     return 0;
 }
---
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+make clean
+make
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+
+
