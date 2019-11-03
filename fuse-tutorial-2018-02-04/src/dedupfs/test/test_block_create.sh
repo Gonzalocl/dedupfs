574268e74032a4bb0661ba14214222b4bee8b82f
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-03 19:31:05 +0100

    change okteta with hexdump

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.sh
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.sh
@@ -5,10 +5,10 @@
 make clean
 make
 bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
-okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
 bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
-okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
 bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
-okteta bin/fs/blo/0011/2233445566778899aabbccddeeff
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
 
 
