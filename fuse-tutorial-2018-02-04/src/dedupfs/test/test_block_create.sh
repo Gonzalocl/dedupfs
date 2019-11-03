33ca690da5cb0ea0557fa9c0460528891963f728
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-03 21:49:14 +0100

    tests

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.sh
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_block_create.sh
@@ -4,11 +4,25 @@
 
 make clean
 make
-bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
-hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
-bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
-hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
+
 bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
 hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
 
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
+
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
+
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddee00 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddee00
+
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddee00 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddee00
+
+bin/test_block_delete blo 5 1 16 1 2 4 00112233445566778899aabbccddeeff 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddeeff
+
+bin/test_block_create blo 5 1 16 1 2 4 00112233445566778899aabbccddee00 0123456789abcdef9876543210fedcba
+hexdump -C bin/fs/blo/0011/2233445566778899aabbccddee00
 
