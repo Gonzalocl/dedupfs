0f84274006de6b61851d8ad699bfed66c19fa65c
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-10 22:10:15 +0100

    file handler

--- fuse-tutorial-2018-02-04/src/dedupfs/file_handler.h
+++ fuse-tutorial-2018-02-04/src/dedupfs/file_handler.h
@@ -9,14 +9,18 @@
 int file_init();
 
 int file_getattr(const char *path, struct stat *stat_buf);
-int file_create(const char *path, mode_t mode);
+int file_mknod(const char *path, mode_t mode);
 int file_unlink(const char *path);
 //int file_rename(const char *path, const char *new_path);
 //int file_link(const char *path, const char *new_path);
 int file_truncate(const char *path, off_t new_size);
-int file_open(const char *path, char *mode);
-
-
-
+int file_open(const char *path, const char *mode);
+int file_read(int fd, void *buf, size_t size, off_t offset);
+int file_write(int fd, const void *buf, size_t size, off_t offset);
+//int file_flush(int fd);
+int file_release(int fd);
+int file_fsync(int fd);
+int file_ftruncate(int fd, off_t offset);
+int bb_fgetattr(int fd, struct stat *stat_buf);
 
 #endif //FILE_HANDLER_H
