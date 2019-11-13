75b67666457e1dc97871aedcf48faa119a8721c7
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-14 00:06:10 +0100

    static test

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_static_block_read_conf.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_static_block_read_conf.c
@@ -5,24 +5,15 @@
 #include <errno.h>
 #include <string.h>
 #include "../block_handler.h"
+#include "../block_handler.c"
 
-extern struct block_handler_conf* handler_conf;
-extern char fs_path[PATH_MAX];
-extern int write_conf_file();
 
 
 int main () {
 
     handler_conf = malloc(sizeof(struct block_handler_conf));
-    strcpy(handler_conf->blocks_path, "test_blocks");
-    handler_conf->block_size = 25;
-    handler_conf->hash_type = 1;
-    handler_conf->hash_length = 16;
-    handler_conf->hash_split = 1;
-    handler_conf->hash_split_size = 2;
-    handler_conf->bytes_link_counter = 4;
     strcpy(fs_path, "bin/fs");
-    int ret = write_conf_file();
+    int ret = read_conf_file("test_blocks");
 
     printf("ret = %d\n", ret);
     printf("\n");
@@ -31,7 +22,7 @@ int main () {
     printf("hash_type:          %d\n", handler_conf->hash_type);
     printf("hash_length:        %d\n", handler_conf->hash_length);
     printf("hash_split:         %d\n", handler_conf->hash_split);
-    printf("hash_split_size     %d\n", handler_conf->hash_split_size);
+    printf("hash_split_size:    %d\n", handler_conf->hash_split_size);
     printf("bytes_link_counter: %d\n", handler_conf->bytes_link_counter);
 
 
--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_static_block_write_conf.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_static_block_write_conf.c
@@ -5,17 +5,22 @@
 #include <errno.h>
 #include <string.h>
 #include "../block_handler.h"
+#include "../block_handler.c"
 
-extern struct block_handler_conf* handler_conf;
-extern char fs_path[PATH_MAX];
-extern int read_conf_file(char *blocks_path);
 
 
 int main () {
 
     handler_conf = malloc(sizeof(struct block_handler_conf));
+    strcpy(handler_conf->blocks_path, "test_blocks");
+    handler_conf->block_size = 25;
+    handler_conf->hash_type = 1;
+    handler_conf->hash_length = 16;
+    handler_conf->hash_split = 1;
+    handler_conf->hash_split_size = 2;
+    handler_conf->bytes_link_counter = 4;
     strcpy(fs_path, "bin/fs");
-    int ret = read_conf_file("test_blocks");
+    int ret = write_conf_file();
 
     printf("ret = %d\n", ret);
     printf("\n");
@@ -24,7 +29,7 @@ int main () {
     printf("hash_type:          %d\n", handler_conf->hash_type);
     printf("hash_length:        %d\n", handler_conf->hash_length);
     printf("hash_split:         %d\n", handler_conf->hash_split);
-    printf("hash_split_size     %d\n", handler_conf->hash_split_size);
+    printf("hash_split_size:    %d\n", handler_conf->hash_split_size);
     printf("bytes_link_counter: %d\n", handler_conf->bytes_link_counter);
 
 
