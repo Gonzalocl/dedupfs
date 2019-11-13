2ec020eeb7cb0eeb074dfaffa71b9b6966bd6d72
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-13 20:06:39 +0100

    remove global variable

--- fuse-tutorial-2018-02-04/src/dedupfs/file_handler.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/file_handler.c
@@ -1,23 +1,24 @@
 #include "file_handler.h"
 
-struct file_handler_conf *handler_conf = NULL;
-char fs_path[PATH_MAX];
 
 
-int file_handler_init(const char *fs, struct file_handler_conf *conf) {
+int file_handler_init(struct file_handler_conf *conf) {
     return 0;
 }
 
-int file_getattr(const char *path, struct stat *stat_buf);
-int file_mknod(const char *path, mode_t mode);
-int file_unlink(const char *path);
-int file_truncate(const char *path, off_t new_size);
-int file_open(const char *path, int flags);
-int file_read(int fd, void *buf, size_t size, off_t offset);
-int file_write(int fd, const void *buf, size_t size, off_t offset);
-int file_release(int fd);
-int file_fsync(int fd);
-int file_ftruncate(int fd, off_t offset);
-int file_fgetattr(int fd, struct stat *stat_buf);
+int file_getattr(struct file_handler_conf *conf, const char *path, struct stat *stat_buf);
+int file_mknod(struct file_handler_conf *conf, const char *path, mode_t mode);
+int file_unlink(struct file_handler_conf *conf, const char *path);
+//int file_rename(struct file_handler_conf *conf, const char *path, const char *new_path);
+//int file_link(struct file_handler_conf *conf, const char *path, const char *new_path);
+int file_truncate(struct file_handler_conf *conf, const char *path, off_t new_size);
+int file_open(struct file_handler_conf *conf, const char *path, int flags);
+int file_read(struct file_handler_conf *conf, int fd, void *buf, size_t size, off_t offset);
+int file_write(struct file_handler_conf *conf, int fd, const void *buf, size_t size, off_t offset);
+//int file_flush(struct file_handler_conf *conf, int fd);
+int file_release(struct file_handler_conf *conf, int fd);
+int file_fsync(struct file_handler_conf *conf, int fd);
+int file_ftruncate(struct file_handler_conf *conf, int fd, off_t offset);
+int file_fgetattr(struct file_handler_conf *conf, int fd, struct stat *stat_buf);
 
 
--- fuse-tutorial-2018-02-04/src/dedupfs/file_handler.h
+++ fuse-tutorial-2018-02-04/src/dedupfs/file_handler.h
@@ -9,6 +9,7 @@
 #define DEFAULT_FILES_PATH "files"
 
 struct file_handler_conf {
+    char fs_path[PATH_MAX];
     char files_path[PATH_MAX];
     char blocks_path[PATH_MAX];
     int block_size;
@@ -21,21 +22,21 @@ struct file_handler_conf {
 
 
 
-int file_handler_init(const char *fs, struct file_handler_conf *conf);
+int file_handler_init(struct file_handler_conf *conf);
 
-int file_getattr(const char *path, struct stat *stat_buf);
-int file_mknod(const char *path, mode_t mode);
-int file_unlink(const char *path);
-//int file_rename(const char *path, const char *new_path);
-//int file_link(const char *path, const char *new_path);
-int file_truncate(const char *path, off_t new_size);
-int file_open(const char *path, int flags);
-int file_read(int fd, void *buf, size_t size, off_t offset);
-int file_write(int fd, const void *buf, size_t size, off_t offset);
-//int file_flush(int fd);
-int file_release(int fd);
-int file_fsync(int fd);
-int file_ftruncate(int fd, off_t offset);
-int file_fgetattr(int fd, struct stat *stat_buf);
+int file_getattr(struct file_handler_conf *conf, const char *path, struct stat *stat_buf);
+int file_mknod(struct file_handler_conf *conf, const char *path, mode_t mode);
+int file_unlink(struct file_handler_conf *conf, const char *path);
+//int file_rename(struct file_handler_conf *conf, const char *path, const char *new_path);
+//int file_link(struct file_handler_conf *conf, const char *path, const char *new_path);
+int file_truncate(struct file_handler_conf *conf, const char *path, off_t new_size);
+int file_open(struct file_handler_conf *conf, const char *path, int flags);
+int file_read(struct file_handler_conf *conf, int fd, void *buf, size_t size, off_t offset);
+int file_write(struct file_handler_conf *conf, int fd, const void *buf, size_t size, off_t offset);
+//int file_flush(struct file_handler_conf *conf, int fd);
+int file_release(struct file_handler_conf *conf, int fd);
+int file_fsync(struct file_handler_conf *conf, int fd);
+int file_ftruncate(struct file_handler_conf *conf, int fd, off_t offset);
+int file_fgetattr(struct file_handler_conf *conf, int fd, struct stat *stat_buf);
 
 #endif //FILE_HANDLER_H
