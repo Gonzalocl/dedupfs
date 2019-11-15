13ca825166dfb5aae47d808247e445748b0193c7
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-15 18:08:20 +0100

    init test

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_file_handler_init.c
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_file_handler_init.c
@@ -1,15 +1,37 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "../file_handler.h"
 
 
-int main () {
+int main (int argc, char *argv[]) {
+
+    if (argc != 9) {
+        printf("ERROR\n");
+        return EXIT_FAILURE;
+    }
+
+    struct file_handler_conf conf;
+    strcpy(conf.fs_path, argv[1]);
+    strcpy(conf.files_path, argv[2]);
+    conf.hash_type = atoi(argv[3]);
+    strcpy(conf.block_handler.blocks_path, argv[4]);
+    conf.block_handler.block_size = atoi(argv[5]);
+    conf.block_handler.hash_split = atoi(argv[6]);
+    conf.block_handler.hash_split_size = atoi(argv[7]);
+
+    unsigned char data[conf.block_handler.block_size];
+    strncpy(data, argv[8], conf.block_handler.block_size);
+
     int ret;
-    ret = file_handler_init(NULL);
-//    printf("init: %d\n", ret);
+    if ((ret = file_handler_init(&conf)) < 0) {
+        printf("ERROR: file_handler_init: %d\n", ret);
+        return EXIT_FAILURE;
+    }
 //    printf("%s:%s:%d\n", __FILE__, __func__, __LINE__);
-    return 0;
+    return EXIT_SUCCESS;
 }
 
 
---
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_file_handler_init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+
+make clean
+make
+
+
+bin/test_file_handler_init bin/fs files 1 blocks 5 1 2 0123456789abcdef9876543210fedcba
+bin/test_file_handler_init bin/fs files 1 blocks 5 1 2 0123456789abcdef9876543210fedcba
+
+
