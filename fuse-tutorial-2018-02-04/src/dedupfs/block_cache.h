771b0f673143c18afe83cfd93dad75068d07b92c
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2019-11-15 22:25:35 +0100

    cache

---
+++ fuse-tutorial-2018-02-04/src/dedupfs/block_cache.h
@@ -0,0 +1,18 @@
+#ifndef BLOCK_CACHE_H
+#define BLOCK_CACHE_H
+
+#include "file_handler.h"
+
+
+struct block_cache;
+
+struct block_cache * cache_init(int fd, int size, struct file_handler_conf *file_handler);
+
+int cache_write(struct block_cache *cache, long block, const void *buf);
+int cache_read(struct block_cache *cache, long block, void *buf);
+
+int cache_end(struct block_cache *cache);
+
+
+
+#endif //BLOCK_CACHE_H
