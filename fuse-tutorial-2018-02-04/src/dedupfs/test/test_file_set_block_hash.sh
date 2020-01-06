8a154369160d96d0956ba341fcc1fcfbef6e6239
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-01-06 10:13:06 +0100

    cehck function

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_file_set_block_hash.sh
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_file_set_block_hash.sh
@@ -4,6 +4,16 @@ COLOR_LOG="\e[36m"
 COLOR_ERR="\e[31m"
 COLOR_RESET="\e[0m"
 
+function check {
+
+if [[ "$1" = "$2" ]]; then
+    echo -e "${COLOR_LOG}$0 OK${COLOR_RESET}"
+else
+    echo -e "${COLOR_ERR}$0 FAILED${COLOR_RESET}" && exit 1
+fi
+
+}
+
 rm -rf bin/fs/*
 make
 
@@ -16,12 +26,7 @@ ok=$(echo "0000000 0000 0000 0000 0000 bbaa bbaa bbaa bbaa
 0000018" | tr -d " ")
 test=$(hexdump bin/fs/files/test_file | tr -d " ")
 
-if [[ "$test" = "$ok" ]]; then
-    echo -e "${COLOR_LOG}$0 OK${COLOR_RESET}"
-else
-    echo -e "${COLOR_ERR}$0 FAILED${COLOR_RESET}" && exit 1
-fi
-
+check "$test" "$ok"
 
 
 bin/test_file_set_block_hash bin/fs files 1 blocks 5 1 2 /test_file 0123456789abcdef9876543210fedcba 1 ccddccddccddccddccddccddccddccdd || ( echo -e "${COLOR_ERR}$0 FAILED${COLOR_RESET}" && exit 1 )
@@ -32,9 +37,5 @@ ok=$(echo "0000000 0000 0000 0000 0000 bbaa bbaa bbaa bbaa
 0000028" | tr -d " ")
 test=$(hexdump bin/fs/files/test_file | tr -d " ")
 
-if [[ "$test" = "$ok" ]]; then
-    echo -e "${COLOR_LOG}$0 OK${COLOR_RESET}"
-else
-    echo -e "${COLOR_ERR}$0 FAILED${COLOR_RESET}" && exit 1
-fi
+check "$test" "$ok"
 
