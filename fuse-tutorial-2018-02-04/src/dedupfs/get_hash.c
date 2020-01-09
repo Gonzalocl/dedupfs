cc0551a3bfa966991c828f3e63855947417c9a43
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-01-09 01:56:48 +0100

    get hash module

---
+++ fuse-tutorial-2018-02-04/src/dedupfs/get_hash.c
@@ -0,0 +1,27 @@
+#include "get_hash.h"
+#include <openssl/evp.h>
+
+// TODO optimize
+int get_hash(int hash_type, int data_size, const char* data, unsigned char *hash) {
+    EVP_MD_CTX *ctx;
+    const EVP_MD *md;
+    int hash_len;
+
+    if (hash_type == 1) {
+        md = EVP_md5();
+    }
+    else if (hash_type == 2) {
+        md = EVP_sha1();
+    }
+    else {
+        return -1;
+    }
+
+    ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, md, NULL);
+    EVP_DigestUpdate(ctx, data, data_size);
+    // TODO check hash len? malloc max mem?
+    EVP_DigestFinal_ex(ctx, hash, &hash_len);
+    EVP_MD_CTX_free(ctx);
+
+}
---
+++ fuse-tutorial-2018-02-04/src/dedupfs/get_hash.h
@@ -0,0 +1,6 @@
+#ifndef GET_HASH_H
+#define GET_HASH_H
+
+int get_hash(int hash_type, int data_size, const char* data, unsigned char *hash);
+
+#endif //GET_HASH_H
