f4348a3f75a8079a0ae63123ebe0c2889d7dedf5
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-01-11 23:26:00 +0100

    one block test

--- fuse-tutorial-2018-02-04/src/dedupfs/test/test_cache_write.sh
+++ fuse-tutorial-2018-02-04/src/dedupfs/test/test_cache_write.sh
@@ -46,7 +46,7 @@ dd if=/dev/urandom of=$block_data bs=1 count=$block_size 2> /dev/null
 block_hash=$($hash_command $block_data | cut -d ' ' -f 1)
 file_size=$block_size
 write_data_size=$(($block_size-2))
-write_data_offset=1
+write_data_offset=0
 write_data=$(mktemp)
 dd if=/dev/urandom of=$write_data bs=1 count=$write_data_size 2> /dev/null
 block=$(($write_data_offset/$block_size))
@@ -56,6 +56,28 @@ bin/test_file_mknod $fs_path $files_path $hash_type $blocks_path $block_size $ha
 bin/test_file_set_size $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path $file_size || exit_error
 bin/test_file_set_block_hash $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path null $block $block_hash || exit_error
 bin/test_block_create $fs_path $blocks_path $block_size $hash_type $hash_length $hash_split $hash_split_size $bytes_link_counter $block_hash $(hexdump -v -e '/1 "%02x"' $block_data) || exit_error
+
+bin/test_cache_write $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path $write_data_size $write_data_offset $(hexdump -v -e '/1 "%02x"' $write_data) || exit_error
+test=$(bin/test_file_get_block_hash $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path null $block null) || exit_error
+
+dd if=$write_data of=$block_data conv=notrunc bs=1 count=$write_data_size seek=$(($write_data_offset%$block_size)) 2> /dev/null
+ok=$($hash_command $block_data | cut -d ' ' -f 1)
+check "$test" "$ok"
+
+test=$(hexdump -v -e '/1 "%02x"' $fs_path/$blocks_path/$(get_path $hash_split $hash_split_size $test))
+ok=$(hexdump -v -e '/1 "%02x"' $block_data)01000000
+check "$test" "$ok"
+
+ls $fs_path/$blocks_path/$(get_path $hash_split $hash_split_size $block_hash) 2>/dev/null > /dev/null && exit_error
+
+
+
+
+
+block_hash=$($hash_command $block_data | cut -d ' ' -f 1)
+write_data_offset=1
+dd if=/dev/urandom of=$write_data bs=1 count=$write_data_size 2> /dev/null
+block=$(($write_data_offset/$block_size))
 
 bin/test_cache_write $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path $write_data_size $write_data_offset $(hexdump -v -e '/1 "%02x"' $write_data) || exit_error
 test=$(bin/test_file_get_block_hash $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path null $block null) || exit_error
@@ -92,6 +114,28 @@ check "$test" "$ok"
 
 ls $fs_path/$blocks_path/$(get_path $hash_split $hash_split_size $block_hash) 2>/dev/null > /dev/null && exit_error
 
+
+
+
+
+block_hash=$($hash_command $block_data | cut -d ' ' -f 1)
+write_data_size=$block_size
+write_data_offset=0
+dd if=/dev/urandom of=$write_data bs=1 count=$write_data_size 2> /dev/null
+block=$(($write_data_offset/$block_size))
+
+bin/test_cache_write $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path $write_data_size $write_data_offset $(hexdump -v -e '/1 "%02x"' $write_data) || exit_error
+test=$(bin/test_file_get_block_hash $fs_path $files_path $hash_type $blocks_path $block_size $hash_split $hash_split_size $path null $block null) || exit_error
+
+dd if=$write_data of=$block_data conv=notrunc bs=1 count=$write_data_size seek=$(($write_data_offset%$block_size)) 2> /dev/null
+ok=$($hash_command $block_data | cut -d ' ' -f 1)
+check "$test" "$ok"
+
+test=$(hexdump -v -e '/1 "%02x"' $fs_path/$blocks_path/$(get_path $hash_split $hash_split_size $test))
+ok=$(hexdump -v -e '/1 "%02x"' $block_data)01000000
+check "$test" "$ok"
+
+ls $fs_path/$blocks_path/$(get_path $hash_split $hash_split_size $block_hash) 2>/dev/null > /dev/null && exit_error
 
 
 
