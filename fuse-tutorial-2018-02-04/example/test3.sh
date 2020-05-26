3ba28e1a6ec10ce95f4a32141a204da26a387261
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-26 22:11:25 +0200

    func

--- fuse-tutorial-2018-02-04/example/test3.sh
+++ fuse-tutorial-2018-02-04/example/test3.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 
-cd ..
-make clean
-make
-cd example
-rm -rf rootdir/*
-../src/bbfs -s rootdir/ mountdir/
 
+mount_dir="mount_dir"
+root_dir="root_dir"
+work_dir="$(pwd)"
 
-fusermount -u mountdir
+# $1: root_dir
+# $2: mount_dir
+# $3: (optional) -s
+# $4: (optional) block-size
+function fs_mount {
+  if [[ $# -eq 4 ]]; then
+    echo ../src/bbfs "$3" --block-size "$4" "$1" "$2"
+  else
+    echo ../src/bbfs "$3" "$1" "$2"
+  fi
+}
+
+# $1: mount_dir
+function fs_umount {
+  fusermount -u "$1"
+}
+
+# clean workspace and build
+function ws_remake {
+  rm -rf rootdir/*
+  cd ..
+  make clean 2> /dev/null > /dev/null
+  make 2> /dev/null > /dev/null
+  cd example
+}
+
 
