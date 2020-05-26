2559a70f239bead6ee1c9d35a953f46044bd609b
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2020-05-26 22:20:55 +0200

    mount and unmount

--- fuse-tutorial-2018-02-04/example/test3.sh
+++ fuse-tutorial-2018-02-04/example/test3.sh
@@ -11,9 +11,9 @@ work_dir="$(pwd)"
 # $4: (optional) block-size
 function fs_mount {
   if [[ $# -eq 4 ]]; then
-    echo ../src/bbfs "$3" --block-size "$4" "$1" "$2"
+    ../src/bbfs "$3" --block-size "$4" "$1" "$2"
   else
-    echo ../src/bbfs "$3" "$1" "$2"
+    ../src/bbfs "$3" "$1" "$2"
   fi
 }
 
@@ -31,4 +31,35 @@ function ws_remake {
   cd example
 }
 
+ws_remake
 
+# test workspace
+test_ws="$(mktemp -d -p . 'tmp.test.XXXX')"
+test_ws="$(realpath $test_ws)"
+
+test_mount_dir="$test_ws/$mount_dir"
+test_root_dir="$test_ws/$root_dir"
+mkdir -p "$test_mount_dir"
+mkdir -p "$test_root_dir"
+
+# reference workspace
+ref_ws="$(mktemp -d -p . 'tmp.ref.XXXX')"
+ref_ws="$(realpath $ref_ws)"
+
+ref_mount_dir="$ref_ws/$mount_dir"
+mkdir -p "$ref_mount_dir"
+
+ref_etc="$ref_ws/etc"
+cp -a /etc "$ref_etc" > /dev/null 2> /dev/null
+
+fs_mount "$test_root_dir" "$test_mount_dir" -s
+
+
+
+
+
+sleep 1
+fs_umount "$test_mount_dir"
+
+rm -rf "$test_ws"
+rm -rf "$ref_ws"
